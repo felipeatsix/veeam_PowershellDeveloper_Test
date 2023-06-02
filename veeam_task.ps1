383ab68ffe15012c0732781015fa0494f58d599e
felipe-santos@outlook.com <felipe-santos@outlook.com>
2023-06-02 10:19:47 +0200

    Update comments

--- veeam_task.ps1
+++ veeam_task.ps1
@@ -2,29 +2,29 @@ function Sync-Directory {
     <#
     .SYNOPSIS
     Synchronizes two folders, source and replica
-    
+
     .DESCRIPTION
     This function will synchronize a source folder within a replica folder on every run.
     If any content is removed from source, that content would be removed from the replica on next run, keeping content exactly the same on every run.
     The script will also copy subfolder structures, but it won't remove subfolders, only its contents if necessary.
-    
+
     .PARAMETER Source
     [MANDATORY] Provides the source folder absolute path
-    
+
     .PARAMETER Destination
     [MANDATORY] Provides the destination folder absolute path
-    
+
     .PARAMETER LogFilePath
     [OPTIONAL] Provides a path and a file name for logging.
     If no path is provided, a new .txt file is going to be auto generated on same path as the script location.
-    
+
     .PARAMETER Force
     [OPTIONAL] It surpasses the confirmation prompt when files needs to be removed from replica folder.
-    
+
     .EXAMPLE
     Sync-Directory -Source c:\source -Destination c:\destination -LogFilePath c:\log.txt
     This command will synchronize folder c:\source within c:\destination and log the whole operation in c:\log.txt
-    
+
     .EXAMPLE
     Sync-Directory -Source c:\source -Destination c:\destination -LogFilePath c:\log.txt -Force
     This command will do exactly the same as example 1, but it will surpass confirmation prompt in case files needs to be removed from destination.
@@ -53,13 +53,13 @@ function Sync-Directory {
         [Parameter(Mandatory = $false)]
         [switch]$Force
     )
-    begin {        
+    begin {
         Write-Verbose "Generating log file: $LogFilePath"
-        
+
         # Open the log file with a filestream instance for both synchronous and asynchronous read and write operations.
         $fileStream = [System.IO.File]::Open($LogFilePath, 'Append', 'Write', 'Read')
         $fileWriter = [System.IO.StreamWriter]::new($fileStream)
-        
+
         # Let's log all script operation
         try {
             Start-Transcript -Path $LogFilePath -Append
@@ -68,23 +68,24 @@ function Sync-Directory {
             Write-Log -ErrorMessage "Error starting transcript: $($_.Exception.Message)"
         }
 
-        # Let's read and count all files on source and destination folders        
+        # Let's read and count all files on source and destination folders
         Write-Verbose -message "Reading source files..."
         $sourceFiles = Get-ChildItem -Path $Source -Recurse -File
         Write-Log -message "Source files count: $($sourceFiles.count)"
-        
+
         Write-Verbose -message "Reading destination files..."
         $destinationFiles = Get-ChildItem -Path $Destination -Recurse -File
         Write-Log -message "Destination files count: $($destinationFiles.count)"
     }
     process {
-        # For each source file that does not exist in destination, copy it over.
-        # If a given file is contained in a subfolder, copy the subfolder first.
-        foreach ($file in $sourceFiles) {            
+        foreach ($file in $sourceFiles) {
+            # For each file in source file, build the destination path for it and then use Get-Item to later verify if it is already synchronized or not
             $destinationPath = Join-Path -Path $destination -ChildPath $file.fullname.replace($source, "")
             $destinationFile = Get-Item -Path $destinationPath -ErrorAction SilentlyContinue
 
+            # Verify file state
             if (-not $destinationFile -or $file.LastWriteTime -gt $destinationFile.LastWriteTime) {
+                # In case file is not in sync state, verify if it is contained within a subfolder, if so, create the folder first
                 $destinationSubfolder = Split-Path -Path $destinationPath -Parent
                 if (-not (Test-Path -Path $destinationSubfolder)) {
                     Write-Log -message "Copy directory $($file.Directory.FullName) => $destinationSubFolder..."
@@ -93,21 +94,24 @@ function Sync-Directory {
                     }
                     catch {
                         Write-Log -ErrorMessage "Failed to create directory: $destinationSubfolder. Error: $($_.Exception.Message)"
-                        throw
+                        # Using return will exit the process block in case of a failure here and move to the end block where we need to do some cleanup operations
+                        return
                     }
                 }
-                # Copy file to the destination folder
+                # Copy file to destination
                 Write-Log -message "Copy file $($file.fullname) => $destinationPath"
                 try {
+                    bla
                     Copy-Item -Path $file.FullName -Destination $destinationPath -Force -ErrorAction Stop
                 }
                 catch {
                     Write-Log -ErrorMessage "Failed to copy file: $($file.fullname) => $destinationPath. Error: $($_.Exception.Message)"
-                    throw
-                }                
+                    # Using return will exit the process block in case of a failure here and move to the end block where we need to do some cleanup operations
+                    return
+                }
             }
         }
-        # For each file that exists in destination but no longer exists in source, remove it.
+        # Remove any files in destination folder that no longer exists in source folder
         foreach ($file in $destinationFiles) {
             Write-Verbose "Verifying files that no longer exists in the source..."
             $sourcePath = Join-Path -Path $source -ChildPath $file.FullName.replace($destination, "")
@@ -119,12 +123,13 @@ function Sync-Directory {
             }
         }
     }
-    end {        
-        # Update the files count after operation is done                
+    end {
+        # Update the files count after operation is done
         $sourceFiles = Get-ChildItem -Path $Source -Recurse -File
         Write-Log -message "Source files final count: $($sourceFiles.count)"
         $destinationFiles = Get-ChildItem -Path $Destination -Recurse -File
-        Write-Log -message "Destination files final count: $($destinationFiles.count)"
+        Write-Log -message "Destination files final count: $($destinationFiles.count)"        
+        # Clean up file streams and stop transcript
         $fileWriter.Dispose()
         $fileStream.Dispose()
         try {
